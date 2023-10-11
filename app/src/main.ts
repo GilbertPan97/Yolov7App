3967974c8c6f41a6828763e3341c1e089aa92ecd
panjiabin <gilbert_pan@outlook.com>
2023-10-11 21:35:03 +0800

    BUG: electron app not valid

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'ant-design-vue/dist/reset.css';
 import Antd from 'ant-design-vue'
-import router from './router/router.ts';
+import router from './router/router';
 
 const app = createApp(App);
 
