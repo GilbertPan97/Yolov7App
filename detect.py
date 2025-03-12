72b49716fe9066e9ac618d2fa3399cfe8c965d59
panjiabin <gilbert_pan@outlook.com>
2025-03-12 21:17:54 +0800

    Rename folder

--- detect.py
+++ detect.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
     # parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
 
     parser.add_argument('--weights', nargs='+', type=str, default='weights/yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='images', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
